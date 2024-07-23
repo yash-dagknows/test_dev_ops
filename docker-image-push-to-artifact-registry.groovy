57aaeefcb2d727f220d8f23769c76c1db589966a
Yash-DagKnows <yash@dagknows.com>
2024-07-23 19:15:27 +0530

    Update docker-image-push-to-artifact-registry.groovy

--- docker-image-push-to-artifact-registry.groovy
+++ docker-image-push-to-artifact-registry.groovy
@@ -11,6 +11,29 @@ pipeline {
     }
 
     stages {
+        stage('Install Docker and gcloud') {
+            steps {
+                script {
+                    sh '''
+                    echo "Installing Docker..."
+                    apt-get update
+                    apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+                    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                    apt-get update
+                    apt-get install -y docker-ce docker-ce-cli containerd.io
+
+                    echo "Starting Docker..."
+                    service docker start
+
+                    echo "Installing gcloud..."
+                    echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+                    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+                    apt-get update && apt-get install -y google-cloud-sdk
+                    '''
+                }
+            }
+        }
         stage('Verify Tools') {
             steps {
                 script {
@@ -24,29 +47,36 @@ pipeline {
                 git branch: 'main', url: "https://github.com/${GITHUB_REPO}.git", credentialsId: "${GITHUB_CREDENTIALS}"
             }
         }
+        stage('Authenticate with Google Cloud') {
+            steps {
+                withCredentials([file(credentialsId: 'gcr-service-account-key', variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
+                    script {
+                        sh '''
+                        echo "Authenticating with Google Cloud..."
+                        gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+                        gcloud auth configure-docker asia-south1-docker.pkg.dev
+                        '''
+                    }
+                }
+            }
+        }
         stage('Build Docker Image') {
             steps {
                 script {
-                    sh """
+                    sh '''
                     echo "Building Docker image..."
                     docker build -t ${GCR_URL}:${BUILD_NUMBER} .
-                    """
+                    '''
                 }
             }
         }
         stage('Push to GCR') {
             steps {
-                withCredentials([file(credentialsId: 'gcr-service-account-key', variable: 'GOOGLE_APPLICATION_CREDENTIALS')]) {
-                    script {
-                        sh """
-                        echo "Authenticating with Google Cloud..."
-                        gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-                        gcloud auth configure-docker asia-south1-docker.pkg.dev
-
-                        echo "Pushing Docker image to GCR..."
-                        docker push ${GCR_URL}:${BUILD_NUMBER}
-                        """
-                    }
+                script {
+                    sh '''
+                    echo "Pushing Docker image to GCR..."
+                    docker push ${GCR_URL}:${BUILD_NUMBER}
+                    '''
                 }
             }
         }
