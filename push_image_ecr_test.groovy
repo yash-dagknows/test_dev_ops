739c44056c4243055e3a7507826e74473333dab0
Yash-DagKnows <yash@dagknows.com>
2024-07-24 12:48:41 +0530

    Update push_image_ecr_test.groovy

--- push_image_ecr_test.groovy
+++ push_image_ecr_test.groovy
@@ -18,6 +18,7 @@ pipeline {
                             string(defaultValue: 'main', description: '<b>Select the desired branch. Ensure the branch name matches with the git branch.</b><br><br>', name: 'Branch'),
                         ])
                     ])
+                    echo "Initialization complete"
                 }
             }
         }
@@ -28,9 +29,10 @@ pipeline {
                     withCredentials([usernamePassword(credentialsId: 'yash-dagknows-github-pat', usernameVariable: 'USERNAME', passwordVariable: 'GIT_TOKEN')]) {
                         sh """
                         git clone -b $Branch https://"\$GIT_TOKEN":x-oauth-basic@github.com/yash-dagknows/test_dev_ops.git
-                        cd $APP_DIR
+                        cd $env.app_dir
                         """
                     }
+                    echo "Source code checked out successfully"
                 }
             }
         }
@@ -41,6 +43,7 @@ pipeline {
                     account_id = '188379622596'
                     aws_region = 'us-east-2'
                     aws_role = 'JenkinsBuildRole'
+                    echo "Pipeline variables initialized"
                 }
             }
         }
@@ -49,7 +52,8 @@ pipeline {
             steps {
                 withAWS(roleAccount: "${account_id}", role: "${aws_role}") {
                     script {
-                        sh """#!/bin/bash
+                        sh """
+                        #!/bin/bash
                         export image_tag=\$(aws ssm get-parameter --name "/test_dev_ops/successful-build" --with-decryption --output text --query Parameter.Value)
                         export image_tag_new=\$(sh version.sh \$image_tag)
                         echo \$image_tag_new
@@ -57,9 +61,16 @@ pipeline {
                         /kaniko/executor --context $APP_DIR --dockerfile=Dockerfile --force --destination=public.ecr.aws/n5k3t9x2/test_dev_ops:\$image_tag_new --destination=public.ecr.aws/n5k3t9x2/test_dev_ops:latest --single-snapshot --cache=false --cache-ttl=1h
                         aws ssm put-parameter --name "/test_dev_ops/successful-build" --type "String" --value \$image_tag_new --overwrite
                         """
+                        echo "Image built and pushed successfully. New image tag: \$image_tag_new"
                     }
                 }
             }
+        }
+    }
+
+    post {
+        always {
+            echo 'Pipeline execution is now complete.'
         }
     }
 }
